014994e7729dcc76c5923e7a4202ebd2fdfe8a39
umi2002 <jonathanhoang2002@gmail.com>
2023-04-15 15:52:16 -0400

    Migration to wayland and hyprland.

--- .config/awesome/startup.lua
+++
@@ -1,5 +0,0 @@
-local awful = require("awful")
-
-awful.spawn.with_shell("~/.config/awesome/autorun.sh")
-awful.spawn.with_shell("~/.config/awesome/xset.sh")
-awful.spawn.with_shell("~/.config/awesome/keyboard.sh")
