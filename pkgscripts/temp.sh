4e0a6a594a6df4990b8b3efcb267a2e9e4ebefee
umi2002 <jonathanhoang2002@gmail.com>
2023-09-03 01:44:23 -0400

    Changed hyprpaper config to auto detect monitor

--- pkgscripts/temp.sh
+++ pkgscripts/temp.sh
@@ -1,46 +1,5 @@
 #!/bin/bash
 
-sudo pacman -S --needed - < pkglist.txt
-echo "Pacman packages installed."
-
-# Check if yay is already installed
-if command -v yay &> /dev/null; then
-    echo "yay is already installed."
-    exit 0
-fi
-
-# Update package database
-echo "Updating package database..."
-sudo pacman -Sy
-
-# Install git and base-devel if they are not installed
-if ! command -v git &> /dev/null; then
-    echo "Installing git..."
-    sudo pacman -S --noconfirm git
-fi
-
-if ! pacman -Qg base-devel &> /dev/null; then
-    echo "Installing base-devel..."
-    sudo pacman -S --noconfirm base-devel
-fi
-
-# Clone yay repository and install yay
-echo "Cloning yay repository..."
-git clone https://aur.archlinux.org/yay.git
-
-cd yay || { echo "Failed to enter yay directory."; exit 1; }
-
-echo "Building and installing yay..."
-makepkg -si --noconfirm
-
-# Clean up
-cd .. && rm -rf yay
-
-echo "yay installation complete."
-
-yay -S --needed - < aur_pkglist.txt
-echo "AUR packages installed."
-
 cargo install --git https://github.com/loichyan/nerdfix.git
 cargo install --git https://github.com/vivekmalneedi/veridian.git
 
