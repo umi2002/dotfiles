28eb02c17a286c85541c482908ea812ba1c9edcd
umi2002 <jonathanhoang2002@gmail.com>
2024-01-26 12:41:57 -0500

    Adjusted minor settings

--- nvim/.config/nvim/lua/umi/settings.lua
+++ nvim/.config/nvim/lua/umi/settings.lua
@@ -9,6 +9,7 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.wrap = false
 
 vim.opt.smartindent = true
 
@@ -25,7 +26,7 @@ vim.opt.termguicolors = true
 vim.opt.scrolloff = 10
 vim.opt.isfname:append("@-@")
 
-vim.opt.updatetime = 750
+vim.opt.updatetime = 500
 
 vim.g.mapleader = " "
 
@@ -87,6 +88,7 @@ end
 vim.opt.list = true
 vim.opt.listchars:append("space:⋅")
 vim.opt.listchars:append("eol:↴")
+vim.opt.listchars:append("tab:| ")
 
 vim.cmd("augroup END")
 
