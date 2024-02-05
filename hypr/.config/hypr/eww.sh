b164cd0d549b834907bb70096c3bd54557140e63
umi2002 <jonathanhoang2002@gmail.com>
2024-02-04 22:13:41 -0500

    Temporary workaround for huge eww logs

--- hypr/.config/hypr/eww.sh
+++ hypr/.config/hypr/eww.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-eww open-many top-left top-center top-right bottom-center volume bottom-left bottom-right
+env XDG_CACHE_HOME=/tmp eww open-many top-left top-center top-right bottom-center volume bottom-left bottom-right
