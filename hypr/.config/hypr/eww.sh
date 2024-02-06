aeaa62e31b861dd61c03cd1692cd0f7cf6c72321
umi2002 <jonathanhoang2002@gmail.com>
2024-02-06 10:45:51 -0500

    Revert "Temporary workaround for huge eww logs"
    This reverts commit b164cd0d549b834907bb70096c3bd54557140e63.

--- hypr/.config/hypr/eww.sh
+++ hypr/.config/hypr/eww.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-env XDG_CACHE_HOME=/tmp eww open-many top-left top-center top-right bottom-center volume bottom-left bottom-right
+eww open-many top-left top-center top-right bottom-center volume bottom-left bottom-right
