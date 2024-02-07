585751475b50b9bf96900e85b5da5879006354a5
umi2002 <jonathanhoang2002@gmail.com>
2024-02-07 14:47:22 -0500

    Fixed song progress scripts

--- eww/.config/eww/scripts/get-art.sh
+++ eww/.config/eww/scripts/get-art.sh
@@ -2,5 +2,4 @@
 
 playerctl -p spotify metadata mpris:artUrl --follow | while read -r art ; do
     curl -o /tmp/spotfy-art.png "$art"
-    eww update song-path=/home/umi/.config/eww/scripts/temp.jpg
-done
+    eww update song-path=/tmp/spotfy-art.png
