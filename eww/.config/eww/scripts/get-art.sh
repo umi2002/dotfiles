981e51d967867a5598bc4304a862efcfe794c1e7
umi2002 <jonathanhoang2002@gmail.com>
2024-02-07 14:30:30 -0500

    Updated where the art is stored

--- eww/.config/eww/scripts/get-art.sh
+++ eww/.config/eww/scripts/get-art.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 playerctl -p spotify metadata mpris:artUrl --follow | while read -r art ; do
-    curl -o /home/umi/.config/eww/scripts/temp.jpg "$art"
+    curl -o /tmp/spotfy-art.png "$art"
     eww update song-path=/home/umi/.config/eww/scripts/temp.jpg
 done
