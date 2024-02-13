d4fdb3d03652a07ca5b2ab51e8240ea3fd98012a
umi2002 <jonathanhoang2002@gmail.com>
2024-02-13 09:43:16 -0500

    Modified config for gitsigns

--- nvim/.config/nvim/lua/umi/lazy.lua
+++ nvim/.config/nvim/lua/umi/lazy.lua
@@ -46,14 +46,7 @@ require("lazy").setup({
 	{
 		"lewis6991/gitsigns.nvim",
 		opts = {
-			-- See `:help gitsigns.txt`
-			signs = {
-				add = { text = "+" },
-				change = { text = "~" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-			},
+			current_line_blame = true,
 		},
 	},
 	"numToStr/Comment.nvim",
