2ab8b05ea8cdff01ae8663f95ad33a74159ffcf7
umi2002 <jonathanhoang2002@gmail.com>
2024-02-15 01:05:23 -0500

    Added editor options

--- emacs/.config/emacs/config.el
+++ emacs/.config/emacs/config.el
@@ -106,3 +106,11 @@
 :font "FiraMono Nerd Font"
 :height 110
 :weight 'medium)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(global-display-line-numbers-mode t)
+(setq display-line-numbers-type 'relative)
+(global-visual-line-mode t)
