0a659890d25045b44c1cb2ba8aa1aede9a0ff455
umi2002 <jonathanhoang2002@gmail.com>
2024-02-16 23:08:32 -0500

    Added keybinds for magit

--- emacs/.config/emacs/config/git/README.el
+++ emacs/.config/emacs/config/git/README.el
@@ -1,1 +1,10 @@
 (use-package magit)
+
+(with-eval-after-load 'magit
+          (with-eval-after-load 'general
+          (leader-key
+        "g" '(:ignore t :wk "Git")
+      "gs" '(magit-status :wk "Git status")
+    "gf" '(magit-pull :wk "Git pull")
+  "gp" '(magit-push :wk "Git push")
+"gb" '(magit-branch :wk "Git branch"))))
