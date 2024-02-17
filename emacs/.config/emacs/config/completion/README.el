879678496a8338141e4fc6dd6e592c387dcc4fc6
umi2002 <jonathanhoang2002@gmail.com>
2024-02-17 00:57:35 -0500

    Added config for company

--- emacs/.config/emacs/config/completion/README.el
+++ emacs/.config/emacs/config/completion/README.el
@@ -1,5 +1,10 @@
 (use-package company)
 
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      company-idle-delay 0.0
+      company-minimum-prefix-length 1)
+
 (use-package yasnippet)
 (use-package yasnippet-snippets
 :after yasnippet)
