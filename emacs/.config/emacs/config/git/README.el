900aeae406c2a4e02207e18c4028cdc4df1add49
umi2002 <jonathanhoang2002@gmail.com>
2024-02-17 19:55:44 -0500

    Added git gutter signs

--- emacs/.config/emacs/config/git/README.el
+++ emacs/.config/emacs/config/git/README.el
@@ -8,3 +8,11 @@
     "gf" '(magit-pull :wk "Git pull")
   "gp" '(magit-push :wk "Git push")
 "gb" '(magit-branch :wk "Git branch"))))
+
+(use-package git-gutter)
+
+(with-eval-after-load 'git-gutter
+(setq git-gutter:modified-sign "│")
+(setq git-gutter:added-sign "│")
+(setq git-gutter:deleted-sign "│")
+  (global-git-gutter-mode +1))
