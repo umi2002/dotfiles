b4a22ca967c500411aac60c46f8b05daa5b0a31b
umi2002 <jonathanhoang2002@gmail.com>
2024-02-17 20:45:44 -0500

    Added window navigation keybinds

--- emacs/.config/emacs/config/navigation/README.el
+++ emacs/.config/emacs/config/navigation/README.el
@@ -27,3 +27,16 @@
     "bn" '(next-buffer :wk "Next buffer")
     "bp" '(previous-buffer :wk "Previous buffer")
     "br" '(revert-buffer :wk "Reload buffer")))
+
+(global-set-key (kbd "C-h")  'windmove-left)
+(global-set-key (kbd "C-l") 'windmove-right)
+(global-set-key (kbd "C-k")    'windmove-up)
+(global-set-key (kbd "C-j")  'windmove-down)
+
+(with-eval-after-load 'general
+  (leader-key
+    "w" '(:ignore t :wk "Window")
+    "wh" '(split-window-left :wk "Window split left")
+    "wl" '(split-window-right :wk "Window split right")
+    "wj" '(split-window-below :wk "Window split below")
+    "wk" '(split-window-right)))
