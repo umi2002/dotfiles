ca8c1e9aba2c10f9699f5110a3bff4893c010528
umi2002 <jonathanhoang2002@gmail.com>
2024-05-01 16:51:17 -0400

    Relative line numbers in netrw

--- nvim/.config/nvim/ftplugin/netrw.lua
+++ nvim/.config/nvim/ftplugin/netrw.lua
@@ -5,3 +5,4 @@ vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", { silent = true, buffer = true })
 
 vim.keymap.set("n", "h", "<Plug>NetrwBrowseUpDir", { silent = true, buffer = true })
 vim.keymap.set("n", "l", "<Plug>NetrwLocalBrowseCheck", { silent = true, buffer = true })
+vim.cmd([[let g:netrw_bufsettings = 'noma nomod nu nobl nowrap ro']])
