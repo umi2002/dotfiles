bef98ccf33b56f0a8e5a16add19244b960ab8875
umi2002 <jonathanhoang2002@gmail.com>
2024-06-03 08:35:58 -0400

    Installed markdown previewer plugin

--- nvim/.config/nvim/lua/plugins/utils.lua
+++ nvim/.config/nvim/lua/plugins/utils.lua
@@ -148,4 +148,12 @@ return {
 		event = "InsertEnter",
 		config = true,
 	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
 }
