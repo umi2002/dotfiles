c1db5ba6b1c8f741f11701b1b7aec6c825aa4196
umi2002 <jonathanhoang2002@gmail.com>
2024-06-24 11:21:53 -0400

    Enforce Microsoft code formatting style for clang_format

--- nvim/.config/nvim/lua/plugins/none.lua
+++ nvim/.config/nvim/lua/plugins/none.lua
@@ -1,54 +1,55 @@
 return {
-	{
-		"nvimtools/none-ls.nvim",
-		config = function()
-			local null = require("null-ls")
-			local formatting = null.builtins.formatting
-			local diagnostics = null.builtins.diagnostics
+    {
+        "nvimtools/none-ls.nvim",
+        config = function()
+            local null = require("null-ls")
+            local formatting = null.builtins.formatting
+            local diagnostics = null.builtins.diagnostics
 
-			null.setup({
-				on_attach = function(client, bufnr)
-					if client.supports_method("textDocument/formatting") then
-						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-						vim.api.nvim_create_autocmd("BufWritePre", {
-							group = augroup,
-							buffer = bufnr,
-							callback = function()
-								-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-								-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
-								vim.lsp.buf.format()
-							end,
-						})
-					end
-				end,
-				sources = {
-					formatting.stylua.with({
-						filetypes = { "lua" },
-					}),
-					formatting.prettierd.with({
-						filetypes = {
-							"markdown",
-							"css",
-							"scss",
-							"html",
-							"json",
-							"jsonc",
-						},
-					}),
-					formatting.shfmt.with({
-						filetypes = { "bash" },
-					}),
-					formatting.clang_format.with({
-						filetypes = { "c", "cpp", "tpp", "cs", "java" },
-					}),
-					formatting.gersemi.with({
-						filetypes = { "cmake" },
-					}),
-					diagnostics.cmake_lint.with({
-						filetypes = { "cmake" },
-					}),
-				},
-			})
-		end,
-	},
+            null.setup({
+                on_attach = function(client, bufnr)
+                    if client.supports_method("textDocument/formatting") then
+                        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                        vim.api.nvim_create_autocmd("BufWritePre", {
+                            group = augroup,
+                            buffer = bufnr,
+                            callback = function()
+                                -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                                -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+                                vim.lsp.buf.format()
+                            end,
+                        })
+                    end
+                end,
+                sources = {
+                    formatting.stylua.with({
+                        filetypes = { "lua" },
+                    }),
+                    formatting.prettierd.with({
+                        filetypes = {
+                            "markdown",
+                            "css",
+                            "scss",
+                            "html",
+                            "json",
+                            "jsonc",
+                        },
+                    }),
+                    formatting.shfmt.with({
+                        filetypes = { "bash" },
+                    }),
+                    formatting.clang_format.with({
+                        filetypes = { "c", "cpp", "tpp", "cs", "java" },
+                        args = { "-style=Microsoft" },
+                    }),
+                    formatting.gersemi.with({
+                        filetypes = { "cmake" },
+                    }),
+                    diagnostics.cmake_lint.with({
+                        filetypes = { "cmake" },
+                    }),
+                },
+            })
+        end,
+    },
 }
