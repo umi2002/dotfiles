78fcac1afa7bf2997855150601fd4a71643a58e0
umi2002 <jonathanhoang2002@gmail.com>
2025-01-20 12:41:10 -0500

    Ignore node modules when using telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -41,6 +41,7 @@ return {
 			-- I don't want to search in the `.git` directory.
 			table.insert(vimgrep_arguments, "--glob")
 			table.insert(vimgrep_arguments, "!**/.git/*")
+			table.insert(vimgrep_arguments, "!**/node_modules/*")
 
 			require("telescope").setup(opts)
 			local function search_conflicts()
