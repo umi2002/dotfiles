afd77220747754bb658c3a93a5b5bebff829cb8c
umi2002 <jonathanhoang2002@gmail.com>
2025-01-27 10:09:39 -0500

    Added custom pickers for fzf-lua

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -1,19 +1,21 @@
-return {
-	{
-		"ibhagwan/fzf-lua",
-		-- optional for icon support
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		-- or if using mini.icons/mini.nvim
-		-- dependencies = { "echasnovski/mini.icons" },
-		opts = {},
-		config = function(_, opts)
-			local fzf = require("fzf-lua")
+local pickers = require("utils.pickers")
 
-			vim.keymap.set("n", "<leader>f", fzf.files)
-			-- vim.keymap.set("n", "<leader>pf", fzf.projects)
-			vim.keymap.set("n", "<leader>gf", fzf.git_files)
-			vim.keymap.set("n", "<leader>wf", fzf.live_grep)
-			vim.keymap.set("n", "<leader>bf", fzf.buffers)
-		end,
-	},
+return {
+    {
+        "ibhagwan/fzf-lua",
+        -- optional for icon support
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = {},
+        config = function(_, opts)
+            local fzf = require("fzf-lua")
+
+            vim.keymap.set("n", "<leader>f", fzf.files)
+            vim.keymap.set("n", "<leader>pf", pickers.projects_search)
+            vim.keymap.set("n", "<leader>gf", fzf.git_files)
+            vim.keymap.set("n", "<leader>wf", fzf.live_grep)
+            vim.keymap.set("n", "<leader>bf", fzf.buffers)
+            vim.keymap.set("n", "<leader>of", fzf.oldfiles)
+            vim.keymap.set("n", "<leader>cf", pickers.conflicts_search)
+        end,
+    },
 }
---
+++ nvim/.config/nvim/lua/utils/pickers.lua
@@ -0,0 +1,100 @@
+return {
+    projects_search = function()
+        -- Get the project paths from project.nvim
+        local ok, project = pcall(require, "project_nvim")
+        if not ok then
+            vim.notify("project.nvim not found", vim.log.levels.ERROR)
+            return
+        end
+
+        local projects = project.get_recent_projects()
+
+        -- Format the projects for display
+        local formatted_projects = {}
+        for _, path in ipairs(projects) do
+            -- Get the project name from the path
+            local name = vim.fn.fnamemodify(path, ":t")
+            -- Add the formatted entry
+            table.insert(formatted_projects, {
+                name = name,
+                path = path,
+                display = string.format(" %s\t[%s]", name, path),
+            })
+        end
+
+        -- Configure the fzf-lua options
+        local opts = {
+            prompt = "Projects > ",
+            actions = {
+                ["default"] = function(selected)
+                    -- Extract the path from the selected entry
+                    local path = selected[1]:match("%[(.+)%]$")
+                    if path then
+                        vim.cmd("e" .. path)
+                    end
+                end,
+            },
+            -- Format the display
+            format = function(entry)
+                return entry.display
+            end,
+        }
+
+        -- Launch fzf-lua with the projects
+        local fzf = require("fzf-lua")
+        fzf.fzf_exec(function(callback)
+            for _, entry in ipairs(formatted_projects) do
+                callback(entry.display)
+            end
+            callback(nil)
+        end, opts)
+    end,
+
+    conflicts_search = function()
+        local conflicts = {}
+        local job = vim.fn.jobstart("git diff --name-only --diff-filter=U --relative", {
+            on_exit = function(_, error)
+                if error ~= 0 then
+                    print(error)
+                    return
+                end
+
+                if #conflicts == 0 then
+                    print("No conflicts")
+                    return
+                end
+
+                local opts = {
+                    prompt = "Conflicts > ",
+                    actions = {
+                        ["default"] = function(selected)
+                            local path = selected[1]
+                            if path then
+                                vim.cmd("e " .. path)
+                            end
+                        end,
+                    },
+                    -- Format the display
+                    format = function(entry)
+                        return entry.display
+                    end,
+                }
+
+                local fzf = require("fzf-lua")
+                fzf.fzf_exec(function(callback)
+                    for _, entry in ipairs(conflicts) do
+                        callback(entry)
+                    end
+                    callback(nil)
+                end, opts)
+            end,
+            on_stdout = function(_, data)
+                for _, v in ipairs(data) do
+                    if v ~= "" then
+                        table.insert(conflicts, "󰈙 " .. v)
+                    end
+                end
+            end,
+        })
+    end,
+}
