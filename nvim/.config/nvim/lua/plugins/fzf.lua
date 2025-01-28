ee0cbb55e1673b643d87d0fce3c81a905bdee1a8
umi2002 <jonathanhoang2002@gmail.com>
2025-01-27 22:55:30 -0500

    fzf files and live grep now show hidden files

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -1,21 +1,30 @@
 local pickers = require("utils.pickers")
 
 return {
-    {
-        "ibhagwan/fzf-lua",
-        -- optional for icon support
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        opts = {},
-        config = function(_, opts)
-            local fzf = require("fzf-lua")
+	{
+		"ibhagwan/fzf-lua",
+		-- optional for icon support
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			files = {
+				fd_opts = [[--hidden]],
+			},
+			grep = {
+				rg_opts = [[--hidden]],
+			},
+		},
+		config = function(_, opts)
+			local fzf = require("fzf-lua")
 
-            vim.keymap.set("n", "<leader>f", fzf.files)
-            vim.keymap.set("n", "<leader>pf", pickers.projects_search)
-            vim.keymap.set("n", "<leader>gf", fzf.git_files)
-            vim.keymap.set("n", "<leader>wf", fzf.live_grep)
-            vim.keymap.set("n", "<leader>bf", fzf.buffers)
-            vim.keymap.set("n", "<leader>of", fzf.oldfiles)
-            vim.keymap.set("n", "<leader>cf", pickers.conflicts_search)
-        end,
-    },
+			vim.keymap.set("n", "<leader>f", fzf.files)
+			vim.keymap.set("n", "<leader>pf", pickers.projects_search)
+			vim.keymap.set("n", "<leader>gf", fzf.git_files)
+			vim.keymap.set("n", "<leader>wf", fzf.live_grep)
+			vim.keymap.set("n", "<leader>bf", fzf.buffers)
+			vim.keymap.set("n", "<leader>of", fzf.oldfiles)
+			vim.keymap.set("n", "<leader>cf", pickers.conflicts_search)
+
+			fzf.setup(opts)
+		end,
+	},
 }
