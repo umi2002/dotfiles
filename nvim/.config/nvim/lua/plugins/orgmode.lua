25a767f239ab12f29c2c0d37f258f7c36f92ffd6
umi2002 <jonathanhoang2002@gmail.com>
2025-01-29 16:54:53 -0500

    Configured orgmode and added orgmode roam plugin

--- nvim/.config/nvim/lua/plugins/orgmode.lua
+++ nvim/.config/nvim/lua/plugins/orgmode.lua
@@ -1,32 +1,51 @@
 return {
-	{
-		"nvim-orgmode/orgmode",
-		opts = {
-			org_agenda_files = { "~/notes/*.org", "~/notes/*/*.org" },
-			org_default_notes_file = "~/notes/index.org",
-			org_hide_leading_stars = true,
-			org_hide_emphasis_markers = true,
-			calendar_week_start_day = 0,
-			org_agenda_start_on_weekday = 0,
-			org_todo_keyword_faces = {
-				TODO = ":background #569fba :foreground red",
-				DONE = ":foreground green",
-			},
-			mappings = {
-				org = {
-					org_cycle = "<leader><TAB>",
-					org_global_cycle = "<leader><S-TAB>",
-					org_open_at_point = "<CR>",
-				},
-			},
-		},
-		config = function(_, opts)
-			require("orgmode").setup(opts)
-			require("headlines").setup({
-				org = {
-					headline_highlights = false,
-				},
-			})
-		end,
-	},
+    {
+        "nvim-orgmode/orgmode",
+        opts = {
+            org_agenda_files = { "~/notes/*.org", "~/notes/*/*.org" },
+            org_default_notes_file = "~/notes/index.org",
+            org_hide_leading_stars = true,
+            org_hide_emphasis_markers = true,
+            calendar_week_start_day = 0,
+            org_agenda_start_on_weekday = 0,
+            org_todo_keyword_faces = {
+                TODO = ":background #569fba :foreground red",
+                DONE = ":foreground green",
+            },
+            mappings = {
+                org = {
+                    org_cycle = "<leader><TAB>",
+                    org_global_cycle = "<leader><S-TAB>",
+                    org_open_at_point = "<CR>",
+                },
+            },
+        },
+        config = function(_, opts)
+            require("orgmode").setup(opts)
+            require("headlines").setup({
+                org = {
+                    headline_highlights = false,
+                },
+            })
+        end,
+    },
+    {
+        "chipsenkbeil/org-roam.nvim",
+        dependencies = {
+            {
+                "nvim-orgmode/orgmode",
+            },
+        },
+        config = function()
+            require("org-roam").setup({
+                directory = "~/umiwiki",
+                -- optional
+                org_files = {
+                    "~/notes/*.org",
+                    "~/notes/*/*.org",
+                    "~/notes/index.org",
+                },
+            })
+        end,
+    },
 }
