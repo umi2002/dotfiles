585d888ddbe8befff70885ba7dc2141a634ab4de
umi <jonathanhoang2002@gmail.com>
2025-09-18 00:06:11 -0400

    Change neovim project manager

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -19,7 +19,6 @@ return {
 			local fzf = require("fzf-lua")
 
 			vim.keymap.set("n", "<leader>f", fzf.files)
-			vim.keymap.set("n", "<leader>pf", pickers.projects_search)
 			vim.keymap.set("n", "<leader>gf", fzf.git_files)
 			vim.keymap.set("n", "<leader>wf", fzf.live_grep)
 			vim.keymap.set("n", "<leader>bf", fzf.buffers)
--- nvim/.config/nvim/lua/plugins/project.lua
+++ nvim/.config/nvim/lua/plugins/project.lua
@@ -1,42 +1,32 @@
 return {
-	{
-		"DrKJeff16/project.nvim",
-		config = function()
-			require("project").setup({
-				---@usage set to true to disable setting the current-woriking directory
-				--- Manual mode doesn't automatically change your root directory, so you have
-				--- the option to manually do so using `:ProjectRoot` command.
-				manual_mode = false,
-				---@usage Methods of detecting the root directory
-				--- Allowed values: **"lsp"** uses the native neovim lsp
-				--- **"pattern"** uses vim-rooter like glob pattern matching. Here
-				--- order matters: if one is not detected, the other is used as fallback. You
-				--- can also delete or rearangne the detection methods.
-				-- detection_methods = { "lsp", "pattern" }, -- NOTE: lsp detection will get annoying with multiple langs in one project
-				detection_methods = { "lsp", "pattern" },
-				---@usage patterns used to detect root dir, when **"pattern"** is in detection_methods
-				patterns = {
-					".git",
-					"_darcs",
-					".hg",
-					".bzr",
-					".svn",
-					"Makefile",
-					"CMakeLists.txt",
-					"package.json",
-					"angular.json",
-				},
-				---@ Show hidden files in telescope when searching for files in a project
-				show_hidden = false,
-				---@usage When set to false, you will get a message when project.nvim changes your directory.
-				-- When set to false, you will get a message when project.nvim changes your directory.
-				silent_chdir = true,
-				---@usage list of lsp client names to ignore when using **lsp** detection. eg: { "efm", ... }
-				ignore_lsp = {},
-				---@type string
-				---@usage path to store the project history for use in telescope
-				datapath = vim.fn.stdpath("data"),
-			})
-		end,
-	},
+    {
+        "coffebar/neovim-project",
+        opts = {
+            projects = { -- define project roots
+                "~/dotfiles/*",
+                "~/school/inf3995/INF3995-105/embedded_ws",
+                "~/school/inf3995/INF3995-105/station",
+                "~/code/*",
+            },
+            picker = {
+                type = "fzf-lua", -- one of "telescope", "fzf-lua", or "snacks"
+            },
+        },
+        init = function()
+            -- enable saving the state of plugins in the session
+            vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
+        end,
+        dependencies = {
+            { "nvim-lua/plenary.nvim" },
+            -- optional picker
+            { "ibhagwan/fzf-lua" },
+            { "Shatur/neovim-session-manager" },
+        },
+        lazy = false,
+        priority = 100,
+        config = function(_, opts)
+            require("neovim-project").setup(opts)
+            vim.keymap.set("n", "<leader>pf", ":NeovimProjectDiscover<CR>")
+        end,
+    },
 }
