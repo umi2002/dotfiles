139db631c64585389cf643caecfeadc9b94d218f
umi <jonathanhoang2002@gmail.com>
2026-01-04 16:22:57 -0500

    Remove terminal plugin from neovim. Add keybind to open terminal popup in tmux.

--- nvim/.config/nvim/lua/plugins/tmux.lua
+++ nvim/.config/nvim/lua/plugins/tmux.lua
@@ -1,11 +1,18 @@
 return {
 	"christoomey/vim-tmux-navigator",
-	config = function()
-		vim.g.tmux_navigator_no_mappings = 1
-
-		vim.keymap.set("n", "<C-l>", "<CMD> TmuxNavigateRight <CR>")
-		vim.keymap.set("n", "<C-h>", "<CMD> TmuxNavigateLeft <CR>")
-		vim.keymap.set("n", "<C-j>", "<CMD> TmuxNavigateDown <CR>")
-		vim.keymap.set("n", "<C-k>", "<CMD> TmuxNavigateUp <CR>")
-	end,
+	cmd = {
+		"TmuxNavigateLeft",
+		"TmuxNavigateDown",
+		"TmuxNavigateUp",
+		"TmuxNavigateRight",
+		"TmuxNavigatePrevious",
+		"TmuxNavigatorProcessList",
+	},
+	keys = {
+		{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+		{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+		{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+		{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+		{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+	},
 }
