26a67ffa1c2d3795ebddc7787bf5d18007afa222
umi <jonathanhoang2002@gmail.com>
2026-02-18 01:07:16 -0500

    Aconfmgr maintenance

--- aconfmgr/.config/aconfmgr/10-base.sh
+++ aconfmgr/.config/aconfmgr/10-base.sh
@@ -6,6 +6,7 @@ AddPackage base # Minimal package set to define a basic Arch Linux installation
 AddPackage base-devel # Basic tools to build Arch Linux packages
 AddPackage linux # The Linux kernel and modules
 AddPackage linux-firmware # Firmware files for Linux - Default set
+AddPackage linux-firmware-qcom # Firmware files for Linux - Firmware for Qualcomm SoCs
 AddPackage linux-headers # Headers and scripts for building modules for the Linux kernel
 AddPackage dkms # Dynamic Kernel Modules System
 
@@ -35,6 +36,8 @@ AddPackage vi # The original ex/vi text editor
 AddPackage brightnessctl # Lightweight brightness control tool
 AddPackage pipewire # Low-latency audio/video router and processor
 AddPackage pipewire-pulse # Low-latency audio/video router and processor - PulseAudio replacement
+AddPackage pipewire-alsa # Low-latency audio/video router and processor - ALSA configuration
+AddPackage pipewire-libcamera # Low-latency audio/video router and processor - Libcamera support
 AddPackage wget # Network utility to retrieve files from the web
 AddPackage --foreign aconfmgr-git # A configuration manager for Arch Linux
 AddPackage --foreign yay # Yet another yogurt. Pacman wrapper and AUR helper written in go.
